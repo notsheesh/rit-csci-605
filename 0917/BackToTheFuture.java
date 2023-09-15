1159dac30cac8b0a7df6327f9706087b3c97ffc1
ntr0pie <tripathi.shreeshv@gmail.com>
2023-09-14 20:36:56 -0400

    Add instance and static var

--- 0917/BackToTheFuture.java
+++ 0917/BackToTheFuture.java
@@ -7,9 +7,23 @@ import javax.annotation.processing.FilerException;
 import javax.print.attribute.standard.NumberOfDocuments;
 
 public class BackToTheFuture {
+    private static final String dictionaryFileName = "dictionary.txt";
+    private static final String testDictionaryFileName = "test-dictionary.txt";
+    private static final String asciiArtFileName = "ascii-art.txt";
+    private static String[] dictionary;
+    private static int numDictionary;
+    private static final boolean PRETTY_PRINT = false;
+    private static final int NUM_TRIES = 9;
+    private static String[] gamePicture = new String[NUM_TRIES * 2];
+    private static int numberErrors = 0;
+    public static int WORD_LENGTH = 5;
+
     public static void main(String args[]){
         String fileName = testDictionaryFileName;
         if(isLoadDictionary(fileName) && isLoadAsciiArt()){
+            // while(isWordLeft()){
+            //     playGame();
+            // }
             playGame();
         }
     }
